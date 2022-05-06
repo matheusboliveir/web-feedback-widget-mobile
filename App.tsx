dee1700c8a81bcc9f4ed3ae21cb8f0e912079b83
matheusboliveir <theus.braz18@gmail.com>
2022-05-06 20:50:58 -0300

    Criando widget de feedback mobile - aula 04 NLW Return

--- App.tsx
+++ App.tsx
@@ -1,11 +1,34 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {
+  Inter_400Regular,
+  Inter_500Medium,
+  useFonts,
+} from "@expo-google-fonts/inter";
+import AppLoading from "expo-app-loading";
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, View } from "react-native";
+import "react-native-gesture-handler";
+import Widget from "./src/components/Widget";
+import { theme } from "./src/theme";
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    Inter_400Regular,
+    Inter_500Medium,
+  });
+
+  if (!fontsLoaded) {
+    return <AppLoading />;
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View
+      style={{
+        flex: 1,
+        backgroundColor: theme.colors.background,
+      }}
+    >
+      <StatusBar style="light" backgroundColor="transparent" translucent />
+      <Widget></Widget>
     </View>
   );
 }
@@ -13,8 +36,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
